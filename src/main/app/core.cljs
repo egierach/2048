8cb243519b5185d0cdfc64b3dece8c85a31907f4
Eric Gierach <signups.eg@gmail.com>
2019-01-29 17:44:06 -0600

    setting up project

---
+++ src/main/app/core.cljs
@@ -0,0 +1,23 @@
+(ns app.core
+  (:require
+   [reagent.core :as reagent]
+   [re-frame.core :as re-frame]
+   [app.events :as events]
+   [app.views :as views]
+   [app.config :as config]))
+
+
+(defn dev-setup []
+  (when config/debug?
+    (enable-console-print!)
+    (println "dev mode")))
+
+(defn mount-root []
+  (re-frame/clear-subscription-cache!)
+  (reagent/render [views/main-panel]
+                  (.getElementById js/document "app")))
+
+(defn ^:export init []
+  (re-frame/dispatch-sync [::events/initialize-db])
+  (dev-setup)
+  (mount-root))
---
+++ src/main/app/views.cljs
@@ -0,0 +1,7 @@
+(ns app.views
+  (:require
+   [re-frame.core :as rf]))
+
+(defn main-panel []
+  [:section.section
+   [:div.container.has-background-light "hi"]])
